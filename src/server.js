6466e4fdd3f85249a274f993092c2d90654b7ddc
fredericka <frederickatettey10@gmail.com>
2024-04-03 16:03:24 +0000

    app is running

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-const PORT = 4000
-await mongoose.connect ('mongodb+srv://<Fresh_Market_Admin_Project>:<LWtIIk7OyNdpqPGd>@fredericka.bzyjsbq.mongodb.net/?retryWrites=true&w=majority&appName=Fredericka')
+const PORT = process.env.PORT || 8000
 
+await mongoose.connect(process.env.MONGO_URI)
 
 app.listen(PORT, () => {
-console.log('express app is running on 4000')
+console.log(`express app is running on ${PORT}`)
 }
 )
