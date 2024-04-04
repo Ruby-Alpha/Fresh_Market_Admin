c7db95bb045881ecf699d6db1bc2c7773fdaf0c6
Abdul-Aziz Mohammed <159774132+abdulazeezcoding@users.noreply.github.com>
2024-04-04 14:34:10 +0000

    removed comments aziz added

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-// I added this
-import UserRoutes from './routes/user.routes.js';
 
 const app = express()
 
 const PORT = 4000
 await mongoose.connect ('mongodb+srv://<Fresh_Market_Admin_Project>:<LWtIIk7OyNdpqPGd>@fredericka.bzyjsbq.mongodb.net/?retryWrites=true&w=majority&appName=Fredericka')
-
-// and this
-app.use(UserRoutes)
 
 app.listen(PORT, () => {
 console.log('express app is running on 4000')
