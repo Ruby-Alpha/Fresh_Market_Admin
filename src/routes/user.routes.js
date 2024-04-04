218153e0d50377509a1f16a6c7c970238f359f0f
abdulazeezcoding <abdulazizmohammed5103>
2024-04-04 14:24:18 +0000

    create register & login routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import express from "express";
 
-const router = express.Router();
+const router = Router();
 
 // register route
 router.post("/api/users/register", async (req, res, next) => {
@@ -16,11 +15,28 @@ router.post("/api/users/register", async (req, res, next) => {
 
 
 // Login a user
-router.get("/api/users/login", async (req,res,next) => {
+router.post("/api/users/login", async (req, res, next) => {
     try {
-        const user = await userModel.findOne({});
+        // attempt to login
+        const loginResult = await userModel.findOne({email: req.body.email}).exec();
         
+        // check if user is already registered
+        if (!user) {
+            res.status(401).json({message: 'Invalid credentials'});
+        }
+
+        // check if email is correct
+        if (email != loginResult.email) {
+            res.status(401).json({message: 'Incorrect email'});
+        }
+
+        // check if password is correct     
+        if (password != loginResult.password) {
+         res.status(401).json({message: 'Incorrect password'});
+        }
+      
     } catch (error) {
+        res.status(500).json({message: error});
         next(error);
     }
 });
